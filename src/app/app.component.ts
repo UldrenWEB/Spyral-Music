7c13b44e04948bc7a397a2a97b3459b2ad3d45f6
UldrenWEB <uldrenmiguel33@gmail.com>
2024-07-03 02:42:26 -0400

    refactor(guards): Se cambio el servicio de storage que usaba el guard

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,22 +1,24 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { NavigationEnd, Router } from '@angular/router';
 import { MusicPlayerService } from './service/MusicPlayerService';
 import { LoaderService } from './service/LoaderService';
 import { filter } from 'rxjs';
+import { StorageService } from './service/StorageService';
 
 @Component({
   selector: 'app-root',
   templateUrl: 'app.component.html',
   styleUrls: ['app.component.scss'],
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   isMiniPlayerVisible: boolean = false;
   isLoading$ = this.loaderService.isLoading$;
   
   constructor(
       private router: Router,
       private playerService: MusicPlayerService,
-      private loaderService: LoaderService
+      private loaderService: LoaderService,
+      private storageService: StorageService
   ) {
     let songHasBeenPlayed = false;
   
@@ -38,6 +40,10 @@ export class AppComponent {
         this.isMiniPlayerVisible = false;
       }
     });
+  }
+
+  async ngOnInit() {  
+    await this.storageService.init();    
   }
 
 
--- src/app/guards/auth.guard.ts
+++ src/app/guards/auth.guard.ts
@@ -4,18 +4,19 @@ import { Injectable } from '@angular/core';
 import { CanActivate, ActivatedRouteSnapshot, RouterStateSnapshot, UrlTree, Router } from '@angular/router';
 import { Observable } from 'rxjs';
 import { AuthStorageService } from '../service/AuthStorage';
+import { StorageService } from '../service/StorageService';
 
 @Injectable({
   providedIn: 'root'
 })
 export class AuthGuard{
 
-  constructor(private authStorageService: AuthStorageService, private router: Router) { }
+  constructor(private storageService: StorageService, private router: Router) { }
 
   async canActivate(
     route: ActivatedRouteSnapshot,
     state: RouterStateSnapshot): Promise<boolean | UrlTree> {
-    // const token = await this.authStorageService.getToken();
+    const token = this.storageService.get('token');
     if (true) {
       return true;
     } else {
